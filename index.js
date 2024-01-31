28d1c01191c6af125139f607b16919bf3a46cf72
sowndharSiva <sowndharsiva914@gmail.com>
2024-01-31 13:34:02 +0530

    solution

---
+++ index.js
@@ -0,0 +1,39 @@
+const express=require("express");
+const {open}=require("sqlite");
+const sqlite3=require("sqlite3");
+const path=require("path")
+const cors=require("cors");
+const dbPath=path.join(__dirname,"PRODUCT.db")
+
+
+let db=null;
+const app=express()
+
+app.use(cors())
+app.use(express.json())
+const initializeDbAndServer=async()=>{
+    try{
+    db=await open({
+        filename:dbPath,
+        driver:sqlite3.Database
+    })
+
+    app.listen(3000,()=>{
+        console.log("Port is running 3000");
+    })
+    }catch(e){
+        console.log(`Error is ${e.message}`);
+        process.exit(1);
+    }
+}
+
+
+
+app.get("/",async(request,response)=>{
+    const query="SELECT * FROM product WHERE productid=1;";
+    const product=await db.get(query);
+    response.send(product);
+
+})
+
+initializeDbAndServer();
---
+++ mysql.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE product ( productid NOT NULL PRIMARY KEY, name TEXT,price INT);
+
+-- INSERT INTO product(productid,name,price) VALUES(1,"watch",35636),(2,"TV",78890);
+-- SELECT * FROM product;
+-- SELECT * FROM product;
+-- SELECT * FROM product;
+-- SELECT * FROM product where productid=1;
